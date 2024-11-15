e14cbd300981ac7129b76812d04377c30fabd8da
Jorge <jorgeluisas6@gmail.com>
2024-11-15 15:03:17 -0600

    menu cmd y print

--- src/GUI.java
+++ src/GUI.java
@@ -80,6 +80,112 @@ public class GUI extends JFrame {
     
     
     
+    private void comandos(String command) {
+        String[] parts = command.split(" ");
+        switch (parts[0].toLowerCase()) {
+            case "mkdir":
+                if (parts.length == 1) {
+                    print("Ingresar el nombre del directorio");
+                } else {
+                    
+                    print(cmd.mkdir(cmd.getPath() + "/" + parts[1]));
+                }
+                break;
+
+            case "mfile":
+                if (parts.length == 1) {
+                    print("Ingresar el nombre del archivo");
+                    
+                } else {
+                    print(cmd.mfile(cmd.getPath() + "/" + parts[1]));
+                    
+                }
+                break;
+
+            case "rm":
+                if (parts.length == 1) {
+                    print("Ingrese el nombre de carpeta/archivo a eliminar");
+                } else {
+                    File file = new File(cmd.getPath() + "/" + parts[1]);
+                    print(cmd.eliminar(file));
+                }
+                break;
+
+            case "cd":
+                if (parts.length == 1) {
+                    
+                    print("Ingresar el nombre del directorio");
+                } else {
+                    
+                    cmd.cd(parts[1]);
+                }
+                break;
+
+            case "...":
+                cmd.cd("..");
+                break;
+
+                
+            case "dir":
+                print(cmd.listar(cmd.getPath()));
+                break;
+
+                
+            case "date":
+                print(LocalDate.now().toString());
+                break;
+
+            case "time":
+                print(LocalTime.now().toString());
+                break;
+                
+            /*
+                PARA ESCRIBIR EN UN ARCHIVO HAY QUE UBICARSE EN LA CARPETA DEONDE ESTA 
+                EL ARCHIVO (EJEM: cd Prgra) Y LUEGO USAR EL COMANDO wr 
+                (EJEM: wr <archivo> <contenido>)
+            */
+            case "wr":
+                if (parts.length < 3) {
+                    print("Ingrese el nombre del archivo y contenido");
+                } else {
+                    StringBuilder content = new StringBuilder();
+                    for (int i = 2; i < parts.length; i++) {
+                        content.append(parts[i]).append(" ");
+                    }
+                    print(cmd.escribir(content.toString().trim(), cmd.getPath() + "/" + parts[1]));
+                }
+                break;
+            
+            /*
+                PARA LEER UN ARCHIVO HAY QUE UBICARSE EN LA CARPETA DEONDE ESTA 
+                EL ARCHIVO (EJEM: cd Prgra) Y LUEGO USAR EL COMANDO rd 
+                (EJEM: rd <archivo>)
+            */
+                
+                
+            case "rd":
+                if (parts.length == 1) {
+                    print("Ingrese el nombre del archivo a leer");
+                } else {
+                    print(cmd.leer(cmd.getPath() + "/" + parts[1]));
+                }
+                break;
+
+            default:
+                print("Comando no reconocido: " + command);
+                break;
+        }
+        
+        window.append("\n" + cmd.getPath() + ">");
+        lastEditablePosition = window.getText().length(); 
+        
+    }
+
+    private void print(String msg) {
+        window.append("\n" + msg);
+    }
+    
+    
 
 
     public static void main(String[] args) {
